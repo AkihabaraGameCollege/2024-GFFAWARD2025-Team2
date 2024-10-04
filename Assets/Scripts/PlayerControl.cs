847c7d2ab6d1979e6d4aa5f8ba78389c71f036d8
Ueno Shingo <ueno_shingo2002@yahoo.co.jp>
2024-10-04 14:18:44 +0900

    左右移動の実装

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class FPSController : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // オブジェクトの回転を固定
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 入力を取得
+        float x = Input.GetAxisRaw("Horizontal");
+
+        // 移動方向の計算（左右のみ）
+        Vector3 move = transform.right * x;
+
+        // リジットボディを使用して移動
+        Move(move);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        // リジットボディに力を加える
+        rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
     }
 }
