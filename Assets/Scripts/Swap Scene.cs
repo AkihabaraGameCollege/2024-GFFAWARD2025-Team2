2de9da7ab68b19e7ebae7b45be90de1498fa6478
tazawa rei <taza_rei@icloud.com>
2024-10-10 12:07:21 +0900

    タイトル画面からゲーム画面とゲーム終了まで

---
+++ Assets/Scripts/Swap Scene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwapScene : MonoBehaviour
+{
+    //シーンの遷移を指定する。
+    public void OnClickTitleButton()
+    {
+        SceneManager.LoadScene("Title Scene");
+    }
+    public void OnClickExitButton()
+    {
+        Application.Quit();
+        Debug.Log("終わり");
+    }
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+
+}
