85b28db604ddec14398f0e7bbc1405da47f0c6d3
Ueno Shingo <ueno_shingo2002@yahoo.co.jp>
2024-10-10 11:32:37 +0900

    QとRが押されてる間、カメラが左右移動

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -1,33 +1,27 @@
 using UnityEngine;
 
-public class CameraRotation : MonoBehaviour
+public class CompassCamera : MonoBehaviour
 {
-    public Transform target; // 回転の中心となるオブジェクト
+    public Transform target; // 回転の基点となるゲームオブジェクト
+    public float rotationSpeed = 50f; // 回転速度
 
     void Update()
     {
-        // Eキーを押したときの処理
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKey(KeyCode.Q))
         {
-            RotateCamera(-90f); // 右に90度回転
+            // 左に回転
+            RotateAroundTarget(-rotationSpeed * Time.deltaTime);
         }
-
-        // Qキーを押したときの処理
-        if (Input.GetKeyDown(KeyCode.Q))
+        else if (Input.GetKey(KeyCode.E))
         {
-            RotateCamera(90f); // 左に90度回転
+            // 右に回転
+            RotateAroundTarget(rotationSpeed * Time.deltaTime);
         }
     }
 
-    void RotateCamera(float angle)
+    private void RotateAroundTarget(float angle)
     {
-        // ターゲットの位置を基にカメラの新しい位置を計算
-        Vector3 direction = transform.position - target.position; // ターゲットへの方向
-        Quaternion rotation = Quaternion.Euler(0, angle, 0); // Y軸周りの回転
-        direction = rotation * direction; // 新しい方向に回転
-        transform.position = target.position + direction; // 新しい位置を設定
-
-        // ターゲットを向く
-        transform.LookAt(target);
+        // 対象のゲームオブジェクトを中心に回転
+        transform.RotateAround(target.position, Vector3.up, angle);
     }
 }
