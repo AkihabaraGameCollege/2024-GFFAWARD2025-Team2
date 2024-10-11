3cf47a071b7ee025a1121303ec6f6d03a6ef8be1
tazawa rei <taza_rei@icloud.com>
2024-10-11 14:38:28 +0900

    タイトル画面編集

--- Assets/Scripts/BossHPGauge.cs
+++ Assets/Scripts/BossHPGauge.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHPGauge : MonoBehaviour
 {
-    [SerializeField]
-    int HPgauge = 10;
+    [SerializeField] private float HPgauge = 10;
+    public Slider HPgaugeSlider;
     // Start is called before the first frame update
     void Start()
     {
