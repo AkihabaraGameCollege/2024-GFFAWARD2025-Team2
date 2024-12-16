1db6ba4e46a401fd4376882b6a5ec5fc7dde23bb
Ueno Shingo <ueno_shingo2002@yahoo.co.jp>
2024-12-16 10:36:46 +0900

    プロジェクト名の変更・エディター上でのスクリプト名の変更

---
+++ Assets/Scripts/RendererOnOffExample.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+
+public class RendererOnOffExample : MonoBehaviour
+{
+    // “_–Å‚³‚¹‚é‘ÎÛ
+    [SerializeField] private Renderer _target;
+    // “_–ÅŽüŠú[s]
+    [SerializeField] private float _cycle = 1;
+
+    private double _time;
+
+    private void Update()
+    {
+        // “à•”Žž‚ðŒo‰ß‚³‚¹‚é
+        _time += Time.deltaTime;
+
+        // ŽüŠúcycle‚ÅŒJ‚è•Ô‚·’l‚ÌŽæ“¾
+        // 0`cycle‚Ì”ÍˆÍ‚Ì’l‚ª“¾‚ç‚ê‚é
+        var repeatValue = Mathf.Repeat((float)_time, _cycle);
+
+        // “à•”Žžtime‚É‚¨‚¯‚é–¾–Åó‘Ô‚ð”½‰f
+        _target.enabled = repeatValue >= _cycle * 0.5f;
+    }
+}
