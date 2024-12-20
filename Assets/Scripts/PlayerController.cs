dc032df034b596227210263daebf5157c5a7f307
Ueno Shingo <ueno_shingo2002@yahoo.co.jp>
2024-12-20 11:00:00 +0900

    ポーズ機能のバグの修正

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -248,4 +248,16 @@ public class PlayerController : MonoBehaviour
             _isSprinting = false;
         }
     }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        // カウントダウン中は入力を無効にする
+        if (gameDirector.isCountdown)
+        {
+            return;  // カウントダウン中は入力処理を行わない
+        }
+
+
+        _pauseManager.TogglePause();
+    }
 }
